26f7777bcfb8909d4bfc2131f9b10ffefe69ffeb
Felipe Pires <felipegamer649@gmail.com>
2024-03-11 01:30:31 -0300

    fazendo correções

--- script.js
+++ script.js
@@ -94,5 +94,3 @@ const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? []
 const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
 
 loadItens()
-
-console.log
